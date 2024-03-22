1cb9b525d3c026eb39f771cba8cfe4e7c1b4739f
John Lewis <github@jlewis.sh>
2024-03-22 23:06:54 +0100

    feat: bootstrapped cargo and flake setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.556873.tar.gz";
+    rust-overlay = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "https://flakehub.com/f/oxalica/rust-overlay/0.1.1327.tar.gz";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        });
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.pkg-config pkgs.openssl
+            toolchain
+          ];
+        };
+      });
+}
