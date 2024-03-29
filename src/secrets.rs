e69073e554e34f62ea1fb2e08c4943f2515881fa
John Lewis <github@jlewis.sh>
2024-03-29 11:59:31 +0100

    feat: added `asana_workspace_gid` to secrets

--- src/secrets.rs
+++ src/secrets.rs
@@ -4,17 +4,24 @@ use color_eyre::eyre::{Result, WrapErr};
 
 #[derive(Debug, Clone)]
 pub struct Secrets {
-  pub asana_pat:  String,
-  pub pco_app_id: String,
-  pub pco_secret: String,
+  pub asana_pat:           String,
+  pub asana_workspace_gid: u64,
+  pub pco_app_id:          String,
+  pub pco_secret:          String,
 }
 
 impl Secrets {
   pub fn new() -> Result<Self> {
     Ok(Self {
-      asana_pat:  var("ASANA_PAT").wrap_err("ASANA_PAT is not set")?,
-      pco_app_id: var("PCO_APP_ID").wrap_err("PCO_APP_ID is not set")?,
-      pco_secret: var("PCO_SECRET").wrap_err("PCO_SECRET is not set")?,
+      asana_pat:           var("ASANA_PAT").wrap_err("ASANA_PAT is not set")?,
+      asana_workspace_gid: var("ASANA_WORKSPACE_GID")
+        .wrap_err("ASANA_WORKSPACE_GID is not set")?
+        .parse()
+        .wrap_err("ASANA_WORKSPACE_GID cannot be parsed as a number")?,
+      pco_app_id:          var("PCO_APP_ID")
+        .wrap_err("PCO_APP_ID is not set")?,
+      pco_secret:          var("PCO_SECRET")
+        .wrap_err("PCO_SECRET is not set")?,
     })
   }
 }
