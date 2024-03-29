892733dd2dce327990f5c9e1d4f9128959652626
John Lewis <github@jlewis.sh>
2024-03-29 12:02:50 +0100

    feat: added `asana_project_gid` to secrets

--- src/secrets.rs
+++ src/secrets.rs
@@ -6,6 +6,7 @@ use color_eyre::eyre::{Result, WrapErr};
 pub struct Secrets {
   pub asana_pat:           String,
   pub asana_workspace_gid: u64,
+  pub asana_project_gid:   u64,
   pub pco_app_id:          String,
   pub pco_secret:          String,
 }
@@ -18,6 +19,10 @@ impl Secrets {
         .wrap_err("ASANA_WORKSPACE_GID is not set")?
         .parse()
         .wrap_err("ASANA_WORKSPACE_GID cannot be parsed as a number")?,
+      asana_project_gid:   var("ASANA_PROJECT_GID")
+        .wrap_err("ASANA_PROJECT_GID is not set")?
+        .parse()
+        .wrap_err("ASANA_PROJECT_GID cannot be parsed as a number")?,
       pco_app_id:          var("PCO_APP_ID")
         .wrap_err("PCO_APP_ID is not set")?,
       pco_secret:          var("PCO_SECRET")
