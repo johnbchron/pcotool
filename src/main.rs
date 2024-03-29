219355b9c42637a729a01155870e08dcbc204988
John Lewis <github@jlewis.sh>
2024-03-29 11:45:30 +0100

    feat: added secrets parsing from env

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use color_eyre::eyre::Result;
 use tracing::instrument;
+
+mod secrets {
+  use std::env::var;
+
+  use color_eyre::eyre::{Result, WrapErr};
+
+  #[derive(Debug, Clone)]
+  pub struct Secrets {
+    pub asana_pat:  String,
+    pub pco_app_id: String,
+    pub pco_secret: String,
+  }
+
+  impl Secrets {
+    pub fn new() -> Result<Self> {
+      Ok(Self {
+        asana_pat:  var("ASANA_PAT").wrap_err("ASANA_PAT is not set")?,
+        pco_app_id: var("PCO_APP_ID").wrap_err("PCO_APP_ID is not set")?,
+        pco_secret: var("PCO_SECRET").wrap_err("PCO_SECRET is not set")?,
+      })
+    }
+  }
+}
 
 fn install_tracing() {
   use tracing_error::ErrorLayer;
@@ -24,6 +47,9 @@ fn install_tracing() {
 async fn main() -> Result<()> {
   install_tracing();
   color_eyre::install()?;
+
+  let secrets = Arc::new(secrets::Secrets::new()?);
+  tracing::info!("loaded secrets");
 
   tracing::info!("making a request to httpbin");
   let resp = reqwest::get("https://httpbin.org/ip")
